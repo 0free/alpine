502bbc967dc6dd3444114e7cd79fc1c6fa00183f
Saif AlSubhi <saifalsubhi@live.com>
2022-09-21 06:08:40 -0700

    Update apkovl.sh

--- apkovl.sh
+++ apkovl.sh
@@ -122,7 +122,7 @@ mkdir -p "$tmp"/etc/profile.d
 makefile root:root 0755 "$tmp"/etc/profile.d/custom.sh <<EOF
 #!/bin/sh
 usermod -s /bin/bash root
-PS1='\[\033[35;1m\]\u\[\033[0m\]@\[\033[31;1m\]\h\[\033[0m\]\[\033[31m\]\$\[\033[0m\] '
+PS1="\[\e[31m\][\[\e[m\]\[\e[38;5;172m\]\u\[\e[m\]@\[\e[38;5;153m\]\w\[\e[m\]\[\e[38;5;214m\]\W\[\e[m\]\[\e[31m\]]\[\e[m\]\\$ "
 alias update='apk update && apk upgrade'
 alias alpine='curl -LO https://raw.githubusercontent.com/0free/alpine/1/install && bash install'
 EOF
