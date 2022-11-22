0caab678b641647eaee1209c2eef7d41b616e32d
Saif AlSubhi <saifalsubhi@live.com>
2022-11-22 11:47:12 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -441,9 +441,9 @@ menu() {
             fi
         done
         read -rsn3 key
-        if [[ $key == '\e[B') ]] && [[ $i -lt ${#options[@]} ]]; then
+        if [[ $key == '\e[B' ]] && [[ $i -lt ${#options[@]} ]]; then
             i=$((i++))
-        elif [[ $key == '\e[A') ]] && [[ $i -gt 0 ]]; then
+        elif [[ $key == '\e[A' ]] && [[ $i -gt 0 ]]; then
             i=$((i--))
         elif [[ $key == '' ]]; then
             break
