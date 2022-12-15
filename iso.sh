945707e42e83af11b3b43f12709e450b738a8729
Saif AlSubhi <saifalsubhi@live.com>
2022-12-15 06:54:44 -0800

    Update iso.sh

--- iso.sh
+++ iso.sh
@@ -51,7 +51,46 @@ profile_linux() {
     kernel_addons='zfs'
     apks="\$apks
         alpine-base alpine-baselayout alpine-baselayout-data alpine-conf alpine-keys alpine-release apk-tools
-        zfs zfs-openrc
+        alsaconf alsa-lib alsa-tools alsa-tools-gui alsa-utils alsa-utils-openrc alsa-plugins-pulse alsa-plugins-jack alsa-ucm-conf
+        bash bash-completion
+        attr binutils bolt coreutils diffutils curl dialog fakeroot findutils gawk grep less nano ncurses-dev net-tools openssl pciutils readline rsync rsync-openrc rsyslog rsyslog-openrc sed shadow sudo usbutils wget which
+        brotli-libs bzip2 lz4 lzo unzip xz zip zlib zstd
+        btrfs-progs btrfs-progs-bash-completion btrfs-progs-extra btrfs-progs-libs
+        dbus dbus-libs dbus-openrc dbus-x11
+        dosfstools e2fsprogs exfatprogs f2fs-tools gptfdisk hfsprogs jfsutils lvm2 mmc-utils mtools ntfs-3g ntfs-3g-progs sfdisk sgdisk squashfs-tools udftools udisks2 udisks2-bash-completion xfsprogs
+        elogind elogind-bash-completion elogind-openrc
+        ethtool ethtool-bash-completion
+        eudev eudev-libs eudev-openrc
+        font-noto-arabic
+        git git-bash-completion
+        hwids-net hwids-pci hwids-udev hwids-usb
+        ibus ibus-bash-completion
+        iptables iptables-openrc
+        iwd iwd-openrc wireless-regdb
+        linux-pam
+        linux-firmware-amd linux-firmware-amd-ucode linux-firmware-amdgpu linux-firmware-i915 linux-firmware-intel linux-firmware-other linux-firmware-rtl_bt linux-firmware-rtl_nic linux-firmware-rtlwifi
+        mesa mesa-dri-gallium
+        musl musl-locales musl-utils
+        networkmanager networkmanager-bash-completion networkmanager-common networkmanager-elogind networkmanager-openrc networkmanager-wifi
+        openrc openrc-bash-completion openrc-settingsd openrc-settingsd-openrc
+        polkit polkit-common polkit-elogind polkit-elogind-libs polkit-openrc
+        gdm gdm-openrc mutter mutter-schemas
+        gnome-desktop gnome-desktop-lang
+        gnome-session gnome-shell gnome-shell-schemas
+        gnome-control-center gnome-control-center-bash-completion
+        gnome-tweaks gnome-shell-extensions arc-dark-gnome
+        gsettings-desktop-schemas
+        pinentry-gnome
+        pipewire pipewire-libs pipewire-alsa pipewire-jack pipewire-pulse pipewire-tools pipewire-spa-tools pipewire-spa-vulkan pipewire-spa-bluez pipewire-media-session wireplumber
+        gnome-terminal gnome-disk-utility gnome-system-monitor gedit
+        nautilus
+        network-manager-applet
+        adwaita-icon-theme hicolor-icon-theme
+        udev-init-scripts udev-init-scripts-openrc
+        util-linux util-linux-bash-completion util-linux-login util-linux-misc util-linux-openrc
+        xauth xinit xkbcomp xkeyboard-config xorg-server xorg-server-common xwayland
+        xf86-input-evdev xf86-input-mtrack xf86-input-synaptics
+        zfs zfs-openrc zfs-libs
     "
 }
 EOF
